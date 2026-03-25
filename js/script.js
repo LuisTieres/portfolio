4c5a30ec94eeb72155b81f037769a0344dd8c289
Luis Tieres <luistieres@gmail.com>
2026-03-24 23:10:34 -0300

    Alterando css

--- js/script.js
+++ js/script.js
@@ -228,7 +228,7 @@ const translations = {
         databases: "Bancos de Dados",
         tools: "Ferramentas",
 
-        hobbies: "Curiosidades",
+        hobbies: "Outros",
         Run: "Corrida",
         runnn1: "Melhor brasileiro na maratona de Helsinque",
         runnn2: "Terceiro lugar geral na Corrida Duque de Caxias",
